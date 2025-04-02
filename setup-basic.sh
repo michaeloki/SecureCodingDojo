ab00ef084f2b8d473be733c7c697d702b41047b4
Michael Oki <michaeloki@Michaels-MacBook-Pro.local>
2025-04-02 18:11:36 +0200

    Fixed config.json line in the setup file

--- setup-basic.sh
+++ setup-basic.sh
@@ -3,6 +3,6 @@ echo "Setting up basic dojo config for the first time"
 mkdir ~/dojofiles
 echo "export DATA_DIR=~/dojofiles" >> ~/.bash_profile
 echo "export CHALLENGE_MASTER_SALT=$(openssl rand -hex 32)" >> ~/.bash_profile
-cp build/trainingportal/config.json ~/dojofiles/config.json
+cp trainingportal/config.json ~/dojofiles/config.json
 chmod +x ./run-basic.sh
 ./run-basic.sh
